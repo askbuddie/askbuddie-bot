401f5528a11b9002e09d364e9333d0724675577f
ashiishme <iamashiishy@gmail.com>
2021-05-24 00:13:40 +0545

    refactor: init Ask Buddie bot & setup flag handler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("Hello from Ask Buddie Bot");
+import AskBuddieBot from 'src/libs/askbuddiebot';
+
+const askBuddieBot = new AskBuddieBot();
+
+// initialize our bot to load all the flags and authorize
+askBuddieBot.initialize();
+
+askBuddieBot.once('ready', () => {
+    console.log('Ask Buddie bot is ready.');
+});
+
+// basic flag handler with message event
+askBuddieBot.on('message', (message) => {
+    const content = message.content;
+    const prefix = askBuddieBot.prefix.find((p) => content.startsWith(p));
+
+    if (!prefix || message.author.bot) return;
+
+    const args = content.slice(prefix.length).trim().split(/ +/);
+    const flag = args.shift()?.toLowerCase();
+
+    if (!askBuddieBot.flagList.has(flag)) return;
+
+    try {
+        console.log(askBuddieBot.flagList.get(flag)?.execute(message));
+    } catch (error) {
+        console.error(error);
+        message.reply('Error executing the command!');
+    }
+});
